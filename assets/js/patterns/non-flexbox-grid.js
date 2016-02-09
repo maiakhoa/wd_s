e3e61f9dad23d489dafeb818eaaa4fc9b864ac09
Damon Cook <damonacook@gmail.com>
2016-02-09 12:27:32 -0500

    add Non-Flexbox Grid to PL and wd_s

---
+++ assets/js/patterns/non-flexbox-grid.js
@@ -0,0 +1,47 @@
+/**
+ * Match Height
+ */
+window.WDS_Match_Height = {};
+( function( window, $, that ) {
+
+	// Constructor.
+	that.init = function() {
+		that.cache();
+
+		if ( that.meetsRequirements ) {
+			that.bindEvents();
+		}
+	};
+
+	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $(window),
+			gridItem: $( '.grid-item' ),
+		};
+	};
+
+	// Combine all events.
+	that.bindEvents = function() {
+		that.$c.window.on( 'load', that.matchGridItems );
+	};
+
+	// Do we meet the requirements?
+	that.meetsRequirements = function() {
+		return that.$c.gridItem.length;
+	};
+
+	// Match the height of all the cards.
+	that.matchGridItems = function() {
+		that.$c.gridItem.matchHeight({
+			byRow: true,
+			property: 'height',
+			target: null,
+			remove: false
+		});
+	};
+
+	// Engage!
+	$( that.init );
+
+})( window, jQuery, window.WDS_Match_Height );
