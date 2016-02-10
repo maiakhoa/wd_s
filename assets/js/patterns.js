b14657f35ebee101342b69842cf8f5fda0d2ceaf
Damon Cook <damonacook@gmail.com>
2016-02-10 13:45:02 -0500

    change JS patterns/ to patterns-src/

--- assets/js/patterns.js
+++ assets/js/patterns.js
@@ -1,2 +1,2 @@
-mobile_navigation = require('./patterns/mobile-navigation.js');
-non_flexbox_grid = require('./patterns/non-flexbox-grid.js');
+mobile_navigation = require('./patterns-src/mobile-navigation.js');
+//non_flexbox_grid = require('./patterns/non-flexbox-grid.js');
