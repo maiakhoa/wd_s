ba015cedf84e59ed0be56b06770667eaf64dc990
Damon Cook <damonacook@gmail.com>
2016-02-11 09:14:32 -0500

    require patterns and assign to window object

--- assets/js/patterns.js
+++ assets/js/patterns.js
@@ -1,2 +1,5 @@
-mobile_navigation = require('./patterns-src/mobile-navigation.js');
-//non_flexbox_grid = require('./patterns/non-flexbox-grid.js');
+// this is mostly for Pattern Library, as WP defines on its own
+//global.jQuery = require('jquery');
+
+window.mobile_navigation = require('./patterns-src/mobile-navigation.js');
+window.non_flexbox_grid = require('./patterns-src/non-flexbox-grid.js');
