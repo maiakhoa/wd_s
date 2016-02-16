44834049811bf013e754495043480b053ca84171
Damon Cook <damonacook@gmail.com>
2016-02-16 15:43:46 -0500

    remove unused 'pattern-library' task from gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -411,7 +411,6 @@ gulp.task('clean:pot', function() {
 gulp.task('i18n', ['clean:pot','wp-pot']);
 gulp.task('icons', ['clean:icons', 'svg']);
 gulp.task('jekyll', ['jekyll-browser-sync', 'jekyll-watch']);
-gulp.task('pattern-library', ['jekyll-browser-sync', 'pl-watch']);
 gulp.task('styles', ['clean:styles', 'postcss', 'cssnano']);
 gulp.task('scripts', ['clean:scripts', 'uglify']);
 gulp.task('sprites', ['imagemin', 'spritesmith']);
