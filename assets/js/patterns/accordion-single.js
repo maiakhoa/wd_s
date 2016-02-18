18a7124e05d523242cdf1d592f32eeafe2fa9fc1
Damon Cook <damonacook@gmail.com>
2016-02-18 17:11:07 -0500

    adding Accordion Single pattern to PL, but JS needs fixing on CodePen

---
+++ assets/js/patterns/accordion-single.js
@@ -0,0 +1,64 @@
+window.SupportAccordionContent = {};
+( function( window, $, that ) {
+
+	// Constructor.
+	that.init = function() {
+		that.cache();
+
+		if ( that.meetsRequirements ) {
+			that.bindEvents();
+			that.toggleSections();
+		}
+	};
+
+	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $( window ),
+			accordionTrigger: $( '.accordion-trigger' ),
+			htmlBody: $( 'html, body' ),
+		};
+	};
+
+	// Combine all events.
+	that.bindEvents = function() {
+		that.$c.window.on( 'load', that.clickAccordionButton );
+	};
+
+	// Do we meet the requirements?
+	that.meetsRequirements = function() {
+		return that.$c.accordionTrigger.length;
+	};
+
+	// Listen for a click on our trigger buttons
+	that.clickAccordionButton = function() {
+		$( 'body' ).on( 'click', '.accordion-trigger', that.showHideContent );
+	};
+
+	// Show/hide the corresponding trigger's content
+	that.showHideContent = function() {
+
+		// If this accordion is already displaying its content, remove the class, hide the content, and return
+		if ( $( this ).hasClass( 'active' ) ) {
+			$( '.accordion-trigger' ).removeClass( 'active' );
+			$( this ).parents( 'article' ).find( '.accordion-content' ).slideUp();
+			return;
+		}
+		
+		// Remove activec alss from all triggers
+		$( '.accordion-trigger' ).removeClass( 'active' );
+		
+		// Add active class to THIS trigger
+		$( this ).addClass( 'active' );
+		
+		// Slide all accordion content up
+		$( '.accordion-content' ).slideUp();
+		
+		// Slide THIS accordion content down
+		$( this ).parents( 'article' ).find( '.accordion-content' ).slideToggle();
+	}
+
+	// Engage!
+	$( that.init );
+
+})( window, jQuery, window.SupportAccordionContent );
