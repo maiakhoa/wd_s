a61681cf45387df5a967c10a98769c09ffc95430
Damon Cook <damonacook@gmail.com>
2016-02-18 14:31:09 -0500

    consolidate all patterns js in to /patterns/ dir

--- assets/js/patterns-src/patterns-about.js
+++
@@ -1,4 +0,0 @@
-// this is mostly for Pattern Library, as WP defines on its own
-// global.jQuery = require('jquery');
-
-window.non_flexbox_grid = require('./patterns/non-flexbox-grid.js');
--- assets/js/patterns-src/patterns-home.js
+++
@@ -1,5 +0,0 @@
-// this is mostly for Pattern Library, as WP defines on its own
-//global.jQuery = require('jquery');
-
-window.mobile_navigation = require('./patterns/mobile-navigation.js');
-window.non_flexbox_grid = require('./patterns/non-flexbox-grid.js');
